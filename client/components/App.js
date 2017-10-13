2e447efade6455d158a535f204f58b1525f1afb9
Max Prosper <maxim.yaaman@gmail.com>
2017-10-13 22:42:02 +0300

    Header with Login, Signin and Logout buttons added. Logging out logic implemented and tesed

---
+++ client/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './Header';
+
+const App = (props) => {
+
+
+    return (
+      <div>
+        <Header />
+        {props.children}
+      </div>
+    );
+
+}
+
+export default App;
